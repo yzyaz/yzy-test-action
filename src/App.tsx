ab4be8aa958e9410284edf6cf4b880e0849d9915
yuzhiyong <yuzhiyong@newrank.cn>
2022-03-24 11:47:21 +0800

    wip: 修改文件测试是否可行

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          test actions
+          test actions2
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
